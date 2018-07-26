c8ed4f1856c3164a0d927086849bc4761a42adda
kanneboyinasujatha <41731075+kanneboyinasujatha@users.noreply.github.com>
2018-07-26 20:24:20 +0530

    Create zero.java

---
+++ zero.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter a number");
+	int number=sc.nextInt();
+	if(number>0)
+	System.out.println("number is positive");
+	else if(number<0)
+	System.out.println("number is negative");
+	else
+	System.out.println("number is zero");
+	}
+}
